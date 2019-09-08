367b44aef20fc18d402f7a4584d73af703b8a298
Milton França <miltondsw@gmail.com>
2019-09-08 11:51:15 -0700

    criado config Page.php

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \franca\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new franca\DB\sql();
-	$results = $sql->select("SELECT * FROM tb_users");
+	$page = new Page();
 
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
