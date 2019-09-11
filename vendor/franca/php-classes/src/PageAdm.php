ebfe29420155942f766db26020b62de761f928be
Milton França <miltondsw@gmail.com>
2019-09-11 00:30:08 -0700

    template adm

---
+++ vendor/franca/php-classes/src/PageAdm.php
@@ -0,0 +1,12 @@
+<?php
+    namespace franca;
+
+    class PageAdm extends Page{
+
+        public function __construct($opts = array(), $tpl_dir = "/views/adm/"){
+
+            parent::__construct($opts, $tpl_dir);
+        }
+
+    }
+?>
